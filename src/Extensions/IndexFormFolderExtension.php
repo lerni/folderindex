e42818042a6a970d64c0d1abcfa4c35e37eaf594
Lukas Erni <le@kraftausdruck.ch>
2021-10-09 20:55:08 +0200

    ShowInSearch is unchecked on new folders https://github.com/lerni/folderindex/issues/4

--- src/Extensions/IndexFormFolderExtension.php
+++ src/Extensions/IndexFormFolderExtension.php
@@ -8,12 +8,16 @@ use SilverStripe\Forms\CheckboxField;
 
 class IndexFormFolderExtension extends Extension
 {
-    // public function updateForm(Form &$form)
-    public function updateFormFields(FieldList $fields)
+    public function updateFormFields(FieldList $fields, $controller, $formName, $context)
     {
         // $fields = $form->Fields();
         $ShowInSearchField = CheckboxField::create('ShowInSearch', _t('Kraftausdruck\Extensions\IndexFormFileExtension.ShowInSearch', 'Indexing child files'));
 
+        $record = isset($context['Record']) ? $context['Record'] : null;
+        if (!$record) {
+            // make sure default value true is respected
+            $ShowInSearchField->setValue('true');
+        }
         $permissionTab = $fields->findTab('Editor.Permissions');
         if ($permissionTab) {
             $fields->insertAfter('EditorGroups', $ShowInSearchField);
