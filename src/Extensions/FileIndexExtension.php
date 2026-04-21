25026001cdeae366d1e7770cc848a8daf51cb86c
Lukas Erni <le@kraftausdruck.ch>
2026-04-21 17:16:50 +0200

    harden NoFileIndex, configurable depth, fix typos

--- src/Extensions/FileIndexExtension.php
+++ src/Extensions/FileIndexExtension.php
@@ -2,27 +2,34 @@
 
 namespace Kraftausdruck\Extensions;
 
+use Psr\Log\LoggerInterface;
 use SilverStripe\Assets\File;
 use SilverStripe\Assets\Folder;
 use SilverStripe\Core\Extension;
 use SilverStripe\ORM\DataObject;
-use SilverStripe\Model\List\ArrayList;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Assets\Flysystem\PublicAssetAdapter;
 
+/**
+ * @extends Extension<object>
+ */
 class FileIndexExtension extends Extension
 {
+    private static int $max_parent_lookup_depth = 10;
+
     /**
      * Returns Folder-DataObject causing blocking, otherwise false
      *
      * @return Folder|false
      */
-    public function NoFileIndex()
+    public function NoFileIndex(): Folder|false
     {
         // Check if database is ready before querying
         // See: https://github.com/lerni/folderindex/issues/3
         // See: https://github.com/silverstripe/silverstripe-framework/issues/10332
         if (!DataObject::getSchema()->tablesAreReadyForClass(File::class)) {
-            return new ArrayList([]);
+            return false;
         }
 
         $blockingFolders = Folder::get()->filter(['ShowInSearch' => 0]);
@@ -36,13 +43,25 @@ class FileIndexExtension extends Extension
             // so we iterate up to next parent with ShowInSearch = 0
             $parentIterInstance = $this->owner->parent();
             $counter = 0;
-            $max = 10;
-            while ($parentIterInstance->ShowInSearch and ($counter < $max)) {
+
+            $max = (int) Config::inst()->get(self::class, 'max_parent_lookup_depth');
+
+            while ($parentIterInstance && $parentIterInstance->ShowInSearch && ($counter < $max)) {
                 $parentIterInstance = $parentIterInstance->parent();
                 $counter++;
             }
-            if ($parentIterInstance->ID != 0) {
+
+            if ($parentIterInstance && ((int) $parentIterInstance->ID !== 0) && !$parentIterInstance->ShowInSearch) {
                 return $parentIterInstance;
+            }
+
+            if ($parentIterInstance && $parentIterInstance->ShowInSearch && ($counter >= $max)) {
+                $fileId = (int) $this->owner->ID;
+                $filename = $this->owner->getFilename();
+
+                Injector::inst()->get(LoggerInterface::class)->warning(
+                    "FolderIndex parent lookup reached max depth ({$max}) for file #{$fileId} ({$filename})",
+                );
             }
         }
 
--- src/Extensions/IndexFormFileExtension.php
+++ src/Extensions/IndexFormFileExtension.php
@@ -2,6 +2,7 @@
 
 namespace Kraftausdruck\Extensions;
 
+use SilverStripe\Assets\Folder;
 use SilverStripe\Core\Extension;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\LiteralField;
@@ -12,8 +13,14 @@ class IndexFormFileExtension extends Extension
     {
         $editorTab = $fields->findTab('Editor.Details');
         $record = isset($context['Record']) ? $context['Record'] : null;
-        if ($editorTab && is_object($record->NoFileIndex())) {
-            $message = _t('Kraftausdruck\Extensions\IndexFormFileExtension.NoindexNotification', 'Indexing disabled per parent folder ({parent})!', ['parent' => $record->NoFileIndex()->Title]);
+
+        if (!$editorTab || !$record) {
+            return;
+        }
+
+        $noFileIndex = $record->NoFileIndex();
+        if ($noFileIndex instanceof Folder) {
+            $message = _t('Kraftausdruck\Extensions\IndexFormFileExtension.NoindexNotification', 'Indexing disabled per parent folder ({parent})!', ['parent' => $noFileIndex->Title]);
             $NoIndexNotificationField = LiteralField::create('X-Robots-Tag', '<p class="alert alert-warning">' . $message . '</p>');
             $fields->insertBefore('Title', $NoIndexNotificationField);
         }
