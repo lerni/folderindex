a9799750e6aa4cb9c0fda67887bfbbc02a369e9b
Lukas Erni <le@kraftausdruck.ch>
2026-04-21 11:40:12 +0200

    database-ready 6.1 edition: cover template helper path and simplify

--- src/Extensions/FolderIndexTemplateUtils.php
+++ src/Extensions/FolderIndexTemplateUtils.php
@@ -4,15 +4,14 @@ namespace Kraftausdruck\Extensions;
 
 use SilverStripe\Assets\File;
 use SilverStripe\Assets\Folder;
-use SilverStripe\Core\ClassInfo;
+use SilverStripe\ORM\DataObject;
 use SilverStripe\Model\List\ArrayList;
 use SilverStripe\View\TemplateGlobalProvider;
 
 class FolderIndexTemplateUtils implements TemplateGlobalProvider
 {
-
     private static $utility_functions = [
-        'KraftausdruckFolderIndex'
+        'KraftausdruckFolderIndex',
     ];
 
     public static function get_template_global_variables()
@@ -22,30 +21,28 @@ class FolderIndexTemplateUtils implements TemplateGlobalProvider
 
     public static function KraftausdruckFolderIndex()
     {
+        // Avoid querying assets tables while db:build --flush is still creating columns.
+        if (!DataObject::getSchema()->tablesAreReadyForClass(File::class)) {
+            return new ArrayList([]);
+        }
 
-        $fileClasses = ClassInfo::subclassesFor(File::class);
+        $blockingFolders = Folder::get()->filter(['ShowInSearch' => 0]);
+        $blockingFoldersArr = [];
+        foreach ($blockingFolders as $bf) {
+            $blockingFoldersArr[$bf->ID] = $bf->getFilename();
+        }
 
-        if (count($fileClasses) && ClassInfo::hasTable('File')) {
-            $blockingFolders = Folder::get()->filter(['ShowInSearch' => 0]);
-            $blockingFoldersArr = [];
-            foreach ($blockingFolders as $bf) {
-                $blockingFoldersArr[$bf->ID] = $bf->getFilename();
-            }
-
-            $subBlockingFoldersArr = [];
-            foreach ($blockingFoldersArr as $key => $bf) {
-                unset($blockingFoldersArr[$key]);
-                foreach ($blockingFoldersArr as $key => $value) {
-                    if (strpos($value, $bf) === 0) {
-                        array_push($subBlockingFoldersArr, $key);
-                    }
+        $subBlockingFoldersArr = [];
+        foreach ($blockingFoldersArr as $key => $bf) {
+            unset($blockingFoldersArr[$key]);
+            foreach ($blockingFoldersArr as $key => $value) {
+                if (strpos($value, $bf) === 0) {
+                    array_push($subBlockingFoldersArr, $key);
                 }
             }
-            if (count($subBlockingFoldersArr)) {
-                $blockingFolders = $blockingFolders->exclude(['ID' => $subBlockingFoldersArr]);
-            }
-        } else {
-            $blockingFolders = new ArrayList([]);
+        }
+        if (count($subBlockingFoldersArr)) {
+            $blockingFolders = $blockingFolders->exclude(['ID' => $subBlockingFoldersArr]);
         }
 
         return $blockingFolders;
